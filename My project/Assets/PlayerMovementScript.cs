f2da33b8a5cec6a7ad2fce42ea1035ccd6580397
KamRax123 <kamaugselah@gmail.com>
2023-03-12 06:28:40 +0000

    scuffed player movement, more platforms and repositioned camera

--- My project/Assets/PlayerMovementScript.cs
+++ My project/Assets/PlayerMovementScript.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerMovementScript : MonoBehaviour
 {
+
+    public CharacterController player;
+
+    public float speed = 7f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,16 @@ public class PlayerMovementScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Movement
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        // Check if the character is moving
+        if (direction.magnitude >= 0.1f)
+        {
+            player.Move(direction * speed * Time.deltaTime);
+        }
     }
 }
