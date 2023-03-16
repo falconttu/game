608fd984c252a82efba96fa31fa570341298ff03
KamRax123 <kamaugselah@gmail.com>
2023-03-16 20:50:04 +0000

    Added Smooth Turning

--- My project/Assets/PlayerMovementScript.cs
+++ My project/Assets/PlayerMovementScript.cs
@@ -8,10 +8,14 @@ public class PlayerMovementScript : MonoBehaviour
     // Refrencing the RigidBody
     Rigidbody rb;
     [SerializeField] float PlayerSpeed = 3f;
+    [SerializeField] float SprintSpeed;
     [SerializeField] float JumpForce = 5f;
     [SerializeField] Transform Player;
     public CharacterController PlayerObj;
+
+    // Smooth Turning
     public float SmoothTurn = 0.1f;
+    float SmoothTurnVelocity;
     // Start is called before the first frame update
     void Start()
     {
@@ -30,7 +34,8 @@ public class PlayerMovementScript : MonoBehaviour
         if (direction.magnitude >= 0.1f)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref SmoothTurnVelocity, SmoothTurn);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             //PlayerObj.Move(direction * PlayerSpeed * Time.deltaTime);
         }
@@ -41,6 +46,13 @@ public class PlayerMovementScript : MonoBehaviour
         if (Input.GetKeyDown("space"))
         {
             rb.velocity = new Vector3(rb.velocity.x, JumpForce, rb.velocity.z);
+        }
+
+        // Players Sprinting Mechanic
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            SprintSpeed = 5f;
+            rb.velocity = new Vector3(horizontal * SprintSpeed, rb.velocity.y, vertical * PlayerSpeed);
         }
 
         // Crouching/Stealth Mechanic
