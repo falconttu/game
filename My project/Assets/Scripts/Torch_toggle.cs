ae958895c3b13183fbe9d8b39a8b17789992986f
falconttu <k2155027@kingston.ac.uk>
2023-03-27 14:46:28 +0100

    added functional torch

---
+++ My project/Assets/Scripts/Torch_toggle.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+
+public class Torch_toggle : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public ParticleSystem ParticleSystem;
+    public SphereCollider SphereCollider;
+    public Light PointLight;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SphereCollider.isTrigger)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                ParticleSystem.Play();
+                PointLight.enabled = true;
+            }
+        }
+    }
+
+}
