b08a95d799faa5c0163e5c1e2d4e7aaf6dc9e517
falconttu <k2155027@kingston.ac.uk>
2023-04-05 16:28:35 +0100

    adding failsafe

--- My project/Assets/Scripts/Torch_toggle.cs
+++ My project/Assets/Scripts/Torch_toggle.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.Experimental.GlobalIllumination;
+using UnityEngine.Rendering.VirtualTexturing;
+
 public class Torch_toggle : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -13,10 +15,12 @@ public class Torch_toggle : MonoBehaviour
     public AudioSource AudioSource;
 
     bool Player_in_Range;
+    bool Torch_on;
 
     void Start()
     {
         Player_in_Range = false;
+        Torch_on = false;
     }
 
     // Update is called once per frame
@@ -24,12 +28,17 @@ public class Torch_toggle : MonoBehaviour
     {
         if(Player_in_Range)
         {
-            if (Input.GetKeyDown(KeyCode.E))
-            {
-                ParticleSystem.Play();
-                PointLight.enabled = true;
-                AudioSource.Play();
-            }
+           if (Torch_on == false)
+           {
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    UnityEngine.Debug.Log("E pressed");
+                    ParticleSystem.Play();
+                    PointLight.enabled = true;
+                    AudioSource.Play();
+                    Torch_on = true;
+                }
+           }
         }
     }
 
